dd9f0738cc1f3742823d4646df1bd8b5d6fd8cd7
Myon <myon.cn@gmail.com>
2014-07-03 21:28:36 +0800

    重构代码 & 添加p站

--- myMenu.myon.uc.js
+++ myMenu.myon.uc.js
@@ -4,30 +4,57 @@
 // @description 右键菜单增强自用版
 // @include     *
 // @author      Myon<myon.cn@gmail.com>
-// @downloadURL 
-// @updateURL   
+// @downloadURL https://github.com/iMyon/UC/raw/master/myMenu.myon.uc.js
+// @homePage    https://github.com/iMyon/UC/blob/master/myMenu.myon.uc.js   
 // @icon        http://tb.himg.baidu.com/sys/portrait/item/c339b7e2d3a1b5c4c3a8d726
-// @version     0.1
+// @version     0.2
 // ==/UserScript==
 
-//右键打开avXXX对应的b站网页
-var openBilibili = {
-  init: function(){
-    addItem({
-      id: "context-openBilibili",
-      label: "open bilibili",
-      oncommand: "openBilibili.open(event);"
-    });
+//规则
+//lable为菜单文本
+//url中的{id}在运行时自动由选中数字文本替换
+var rules = [
+  {
+    attr:{
+      label: "bilibili"
+    },
+    url: "http://www.bilibili.com/video/av{id}"
   },
-  open: function(e){
-    var url = "http://www.bilibili.tv/video/";
-    var where = (e.type == "click" && e.button == 1) ? "tabshifted" : "tab";
-    url += content.getSelection();
-    var that = gContextMenu;
-    var doc = that ? that.target.ownerDocument : '';
-    openUILinkIn(url, where, null, null, doc.documentURIObject);
-    closeMenus(e.target);
+  {
+    attr:{
+      label: "pixiv"
+    },
+    url: "http://www.pixiv.net/member_illust.php?mode=medium&illust_id={id}"
   }
+];
+
+var menus = {
+  init: function(){
+    var cacm = document.getElementById("contentAreaContextMenu");
+    if (!cacm) return;
+    var menu = $Element({
+      id: "myMenu",
+      label: "博麗大結界"
+    },"menu");
+    var menupopup = $Element({},"menupopup");
+    for(let i=0;i<this.rules.length;i++){
+      let item = $Element(this.rules[i].attr);
+      (function(item,url){
+        item.addEventListener("command",function(e){
+          var where = (e.type == "click" && e.button == 1) ? "tabshifted" : "tab";
+          var openUrl = url.replace("{id}",(""+content.getSelection()).match(/\d+/)[0]);
+          var that = gContextMenu;
+          var doc = that ? that.target.ownerDocument : '';
+          openUILinkIn(openUrl, where, null, null, doc.documentURIObject);
+          closeMenus(e.target);
+        });
+      })(item,this.rules[i].url);
+      menupopup.appendChild(item);
+    }
+    menu.appendChild(menupopup);
+    cacm.insertBefore(menu,document.getElementById("context-sendimage"));
+  },
+  rules: rules
 };
 
 //github短网址
@@ -62,25 +89,23 @@ var githubShortUrl = {
 };
 
 //添加到右键菜单
-function addItem(option){
-  var cacm = document.getElementById("contentAreaContextMenu");
-    if (!cacm) return;
-  var item = document.createElement("menuitem");
+function $Element(option,name = "menuitem"){
+  var item = document.createElement(name);
   for(var key in option){
     item.setAttribute(key, option[key]);
   }
-  cacm.insertBefore(item,document.getElementById("context-sendimage"));
+  return item;
 }
 
 //各种初始化
 if (window.location == "chrome://browser/content/browser.xul") {
-    openBilibili.init();
+    menus.init();
     githubShortUrl.init();
     document.getElementById("contentAreaContextMenu").addEventListener("popupshowing", function () {
       var c = content.getSelection() + "";
       //满足匹配显示右键菜单，否则隐藏
-      gContextMenu.showItem("context-openBilibili", gContextMenu.isTextSelected 
-        && c.match(/av\d+/));
+      gContextMenu.showItem("myMenu", gContextMenu.isTextSelected 
+        && (c.match(/^\d+$/) || c.match(/^av\d+$/)));
       gContextMenu.showItem("context-githubShortUrl", content.location.href.match(/github.com/));
     }, false);
 }
